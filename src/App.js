91880103ada05aa5ec6c6d732b5f94aacc28aa43
BK <bhushankale1025@gmail.com>
2022-02-17 12:29:56 +0530

    added img

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import bg1 from "./imgs/bg1.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <p>HIII</p>
+    <div className="bghome">
+      <img src={bg1} alt=" "></img>
     </div>
   );
 }
